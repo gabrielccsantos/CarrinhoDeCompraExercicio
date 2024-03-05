58511cc9f197ee788645857dd4496e1ffa7dd942
gabriel santos <gabrielcarneirocsantos@gmail.com>
2024-03-04 21:48:45 -0300

    Teste

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -5,17 +5,19 @@ import org.example.entities.Item;
 import org.example.entities.Product;
 
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
         try{
-            Product p1 = new Product(123, "Tv", "Grande");
-            Product p2 = new Product(123, "Notebook", "Pequeno");
+            Product p = new Product(123, "tv", "teste");
+            Product p1 = new Product(10, "tv", "gelo");
+
+            System.out.println(p.equals(p1));
         }catch (BarCodeEquals e){
             System.out.println(e.getMessage());
         }
-
     }
 }
