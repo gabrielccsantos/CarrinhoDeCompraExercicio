abcce0d55365f3f47a194b3aaf0784d80a4774ed
gabriel santos <gabrielcarneirocsantos@gmail.com>
2024-03-11 22:30:01 -0300

    Teste ok

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,22 +1,33 @@
 package org.example.application;
 
-import org.example.Exceptions.BarCodeEquals;
 import org.example.entities.Item;
 import org.example.entities.Product;
+import org.example.entities.ShoppingCart;
 
 import java.math.BigDecimal;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
         try{
-            Product p = new Product(123, "tv", "teste");
-            Product p1 = new Product(10, "tv", "gelo");
+            Item item1 = new Item(new Product(123,"TV", "Grande"), new BigDecimal(1000), 1);
+            Item item2 = new Item(new Product(321, "microondas", "pequeno"), new BigDecimal(300), 2);
+            Item item3 = new Item(new Product(123, "TV", "Grande"), new BigDecimal(500), 3);
 
-            System.out.println(p.equals(p1));
-        }catch (BarCodeEquals e){
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            shoppingCart.addList(item1);
+            shoppingCart.addList(item2);
+
+            shoppingCart.seeAll();
+            System.out.println("Total = " + shoppingCart.getTotal());
+
+            shoppingCart.addList(item3);
+
+            shoppingCart.seeAll();
+            System.out.println("Total = " + shoppingCart.getTotal());
+
+        }catch (RuntimeException e){
             System.out.println(e.getMessage());
         }
     }
