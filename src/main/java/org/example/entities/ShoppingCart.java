a30e3baad3cd7e738973a8d7a8d60edfc9bcaad8
gabriel santos <gabrielcarneirocsantos@gmail.com>
2024-03-11 22:29:48 -0300

    Fix getTotal e restricoes em adicinar

--- src/main/java/org/example/entities/ShoppingCart.java
+++ src/main/java/org/example/entities/ShoppingCart.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    private List<Item> listItem = new ArrayList<>();
+    private List<Item> listItem;
+
+
+    public ShoppingCart(){
+        listItem = new ArrayList<>();
+    }
 
     public void addList(Item item){
         for(Item i : listItem){
             if(i.getProduct().equals(item.getProduct())){
-               i.setQuantity( i.getQuantity() + item.getQuantity());
-               if(!i.getValueOfUnit().equals(item.getValueOfUnit())){
-                   i.setValueOfUnit(item.getValueOfUnit());
-               }
+                listItem.remove(i);
             }
-            listItem.add(item);
         }
+        listItem.add(item);
     }
 
     public boolean removeItem(Product product){
@@ -30,10 +32,17 @@ public class ShoppingCart {
     }
 
     public BigDecimal getTotal(){
-        BigDecimal total = new BigDecimal("0");
-        for(Item item : listItem){
-            total.add(item.getTotal());
+        int soma = 0;
+        for(Item i : listItem){
+            soma += i.getTotal().intValue();
         }
-        return total;
+
+        return new BigDecimal(soma);
+    }
+
+    public void seeAll(){
+        for (Item item : listItem){
+            System.out.println(item);
+        }
     }
 }
